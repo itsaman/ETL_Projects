3c84cd77001040384abef9e67c67eb88e353205e
Aman gupta <amang4996@gmail.com>
2022-01-21 01:14:42 +0530

    Update lambda_function.py

--- AWS_to_Snowflake/lambda_function.py
+++ AWS_to_Snowflake/lambda_function.py
@@ -6,14 +6,14 @@ from io import StringIO
 def lambda_handler(event, context):
     s3_file_key = event['Records'][0]['s3']['object']['key'];
     bucket = 'sourcefile2001';
-    s3 = boto3.client('s3', aws_access_key_id='AKIAQKNRPXXTBSVKSXA4',  aws_secret_access_key='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')
+    s3 = boto3.client('s3', aws_access_key_id='XXX',  aws_secret_access_key='XXX')
     obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
     initial_df = pd.read_csv(io.BytesIO(obj['Body'].read()));
 
     service_name = 's3'
     region_name = 'ap-south-1'
-    aws_access_key_id = 'AKIAQKNRPXXTBSVKSXA4'
-    aws_secret_access_key = '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'
+    aws_access_key_id = 'XXX'
+    aws_secret_access_key = 'XXX'
 
     s3_resource = boto3.resource(
         service_name=service_name,
